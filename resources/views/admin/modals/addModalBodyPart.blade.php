31ff9f50833deef7ad3d8f17223b88824d644451
rahulanku1902 <rahulanku1902@gmail.com>
2023-11-16 23:23:43 +0530

    Thursday-update

--- resources/views/admin/modals/addModalBodyPart.blade.php
+++ resources/views/admin/modals/addModalBodyPart.blade.php
@@ -29,7 +29,7 @@
                                 <span class="text-danger">*{{ $message }}</span>
                             @enderror
                             <div class="">
-                                <object id="svgObject" style="width:100%;" data="{{ asset('modals_images/'.$modal->side_image) ?? '' }}" type=""></object>
+                                <object class="svgObject" id="" style="width:100%;" data="{{ asset('modals_images/'.$modal->side_image) ?? '' }}" type=""></object>
                             </div>
                         </div>
                     </div>
@@ -41,7 +41,7 @@
                                         <div class="form-group">
                                             <label class="form-label" for="title">Body Part Title</label>
                                             <div class="form-control-wrap">
-                                                <input type="text" class="form-control" id="title" placeholder="Title">
+                                                <input type="text" class="form-control bodyTitle" id="title" placeholder="Title">
                                             </div>
                                         </div>
                                         <div class="form-group">
@@ -56,7 +56,6 @@
                                     </div>
                                 </div>
                             </div>
-                       
 <!--                                     
                                     <button type="button" class="btn btn-outline-dark addAccent" id="addAccent">Add Accent</button>
                                     <button type="button" class="btn btn-outline-danger removeBodyPart">Remove Body Part</button> -->
@@ -79,7 +78,7 @@
     </div>
 </div>
 
-<div class="modal fade" tabindex="-1" id="modalDefault">
+<!-- <div class="modal fade" tabindex="-1" id="modalDefault">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <a href="#" class="close" data-bs-dismiss="modal" aria-label="Close">
@@ -96,9 +95,128 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 
 <script>
+    $(document).ready(function () {
+    var imageSelectionActive = false;
+
+    $("body").on("click", ".selectImageArea", function () {
+    var bodyPart = $(this).closest('.accentCol');
+    var remove = bodyPart.find('.removeAccent');
+    var inputElement = bodyPart.find('input[type="text"]');
+    console.log(inputElement.val());
+    var Pathclass = $(this).attr('data-class');
+    var title = bodyPart.find('input[type="text"].' + Pathclass).val();
+
+    if (title == '') {
+        alert('Please add your title first.');
+        return false;
+    }
+
+    if (title) {
+        var countVal = 0;
+
+        $(".accentCol input[type='text']." + Pathclass + ":disabled").each(function () {
+            if ($(this).val().trim() === title.trim()) {
+                countVal++;
+            }
+        });
+
+        if (countVal > 0) {
+            alert('Please try a unique title.');
+            return false;
+        }
+    }
+    var dinamicClass = title+$(this).attr('data-class');
+    console.log(dinamicClass);
+
+    inputElement.attr('disabled', true);
+    $('.svgObject:first').attr('id', 'svgObject');
+    console.log($(this).html());
+    $(this).html("Done").addClass('doneAreaSelection').removeClass('selectImageArea').attr('dinamicClass',dinamicClass);
+    remove.attr('dinamicClass',dinamicClass);
+    imageSelectionActive = true;
+    $('button').each(function () {
+        if (!$(this).hasClass('doneAreaSelection')) {
+            $(this).prop('disabled', true);
+        }
+    });
+
+        // After setting the ID, wait for a short delay to ensure the content is loaded
+        setTimeout(function () {
+            var svgObject = document.getElementById('svgObject');
+            var svgDoc = svgObject.contentDocument;
+           
+            // Check if the SVG content is available
+            if (svgDoc) {
+                $(svgDoc).find('path').on('click', function () {
+                    if (imageSelectionActive) {
+                        var oldColor = $(this).attr('fill');
+                        if(oldColor == 'red'){
+                            $(this).attr('fill', 'red').addClass(dinamicClass); 
+                        }else{
+                            $(this).attr('fill', 'red').addClass(dinamicClass).attr('old-color',oldColor);
+                        }
+                        
+                    }
+                });
+            } else {
+                console.log('SVG content not yet loaded.');
+            }
+        }, 100);
+    });
+
+    $('body').on("click", ".doneAreaSelection", function () {
+        $('button').each(function () {
+           $(this).prop('disabled', false);
+        });
+        var Dclass = $(this).attr('dinamicClass');
+        var pathsWithDclass = $('path.' + Dclass);
+
+        // Now pathsWithDclass contains all path elements with the specified Dclass
+        console.log('Paths with class ' + Dclass + ':', pathsWithDclass);
+        var svgObject = document.getElementById('svgObject');
+
+        // Wait for a short delay to ensure the content is loaded
+        setTimeout(function () {
+        var svgObject = document.getElementById('svgObject');
+        var svgDoc = svgObject.contentDocument;
+
+        // Check if the SVG content is available
+        if (svgDoc) {
+            var allPaths = $(svgDoc).find('path');
+
+            allPaths.each(function () {
+                if ($(this).hasClass(Dclass)) {
+                    var oldColor = $(this).attr('old-color');
+
+                    $(this).attr('fill', oldColor);
+                }
+            });
+
+            console.log(allPaths);
+        } else {
+            console.log('SVG content not yet loaded.');
+        }
+    }, 100);
+
+
+        imageSelectionActive = false;
+        $(this).hide();
+
+        console.log('Image selection deactivated.');
+    });
+
+    // $('body').on("click", ".doneAreaSelection", function () {
+    //     imageSelectionActive = false;
+    //     var Dclass = $(this).attr('dinamicClass');
+    //     $(this).hide();
+
+    //     console.log('Image selection deactivated.');
+    // });
+});
+
     // $(document).ready(function() {
     //     $('#svgObject').on('load', function() {
     //         var svgDoc = $('#svgObject')[0].contentDocument;
@@ -108,32 +226,86 @@
     //         });
     //     });
     // });
-    $(document).ready(function () {
-    var fillColor = '';
 
-    $('#svgObject').on('load', function () {
-        var svgDoc = $('#svgObject')[0].contentDocument;
+    // $(document).ready(function () {
+    // $("body").on("click", ".selectImageArea", function () {
+    //     console.log("Clicked");
 
-        // Log the 'd' attribute of each path with the class 'body_parts1'
-        $(svgDoc).find('path.body_parts1').each(function () {
-            console.log($(this).attr('d'));
-        });
+    //     $('.svgObject:first').attr('id', 'svgObject');
+    //     console.log('click');
 
-        // Attach click event to all paths
-        $(svgDoc).find('path').on('click', function () {
-            // Store the color of the clicked path
-            fillColor = $(this).attr('fill');
+    //     // After setting the ID, wait for a short delay to ensure the content is loaded
+    //     setTimeout(function () {
+    //         var svgObject = document.getElementById('svgObject');
+    //         var svgDoc = svgObject.contentDocument;
 
-            // Remove class and color from all paths
-            $('path').removeClass('body_parts1').attr('fill', fillColor);
+    //         // Check if the SVG content is available
+    //         if (svgDoc) {
+    //             $(svgDoc).find('path').on('click', function () {
+    //                 $(this).attr('fill', 'red').addClass('body_parts1');
+    //             });
+    //         } else {
+    //             console.log('SVG content not yet loaded.');
+    //         }
+    //     }, 100);
+    // });
 
-            // Add class and color to the clicked path
-            $(this).attr('fill', 'red').addClass('body_parts1');
+    // $("body").on("click", ".svgObject", function () {
+    //     console.log('click..');
+    //     // Handle click on the SVG object if needed
+    // });
+    // $('body').on("click", ".doneAreaSelection", function () {
+    //     svgChangeEnabled = !svgChangeEnabled;
 
-            console.log(fillColor);
-        }); 
-    });
-});
+    //     // Optionally, you can remove the 'svgObject' ID when changes are disabled
+    //     if (!svgChangeEnabled) {
+    //         $('.svgObject:first').removeAttr('id');
+    //     }
+
+    //     console.log('Changes to image paths and class are ' + (svgChangeEnabled ? 'enabled' : 'disabled'));
+    // });
+// });
+
+
+    // $(document).ready(function () {
+    // var fillColor = '';
+
+    // $('#svgObject').on('load', function () {
+    //     var svgDoc = $('#svgObject')[0].contentDocument;
+
+    //     // Log the 'd' attribute of each path with the class 'body_parts1'
+    //     $(svgDoc).find('path.body_parts1').each(function () {
+    //         console.log($(this).attr('d'));
+    //     });
+
+    //     // Attach click event to all paths
+    //     $(svgDoc).find('path').on('click', function () {
+    //         // Store the color of the clicked path
+    //         fillColor = $(this).attr('fill');
+
+    //         // Remove class and color from all paths
+    //         $('path').removeClass('body_parts1').attr('fill', fillColor);
+
+    //         // Add class and color to the clicked path
+    //         $(this).attr('fill', 'red').addClass('body_parts1');
+
+    //         console.log(fillColor);
+    //     }); 
+    // });
+    // $("body").on("click", ".selectImageArea", function() {
+    //     console.log("Clicked"); 
+
+    //     $('.svgObject:first').attr('id', 'svgObject');
+    // });
+    // $("body").on("click", ".svgObject", function () {
+    //         console.log('click..');
+    //         var svgDoc = $('.svgObject')[0].contentDocument;
+            
+    //         $(svgDoc).find('path').on('click', function() {
+    //             $(this).attr('fill', 'red').addClass('body_parts1');
+    //         });
+    //     });
+// });
 
 
     // $(document).ready(function() {
@@ -159,15 +331,63 @@
         $(".bodypartRow").append(newBodyPart);
     });
 
+    
     $("body").on("click", ".removeBodyPart", function () {
-        var bodyPart = $(this).closest('.bodyPart');
-        bodyPart.remove();
-    });
+    var bodyParts = $('.bodyPart');
+    var bodyPart = $(this).closest('.bodyPart');
+    var buttonsRemove = bodyPart.find(".removeAccent");
+
+    // Check if there's only one body part
+    if (bodyParts.length === 1) {
+        alert('Sorry, you cannot remove the last body part.');
+        return;
+    }
+
+    var dynamicClasses = buttonsRemove.map(function () {
+        return $(this).attr('dinamicclass');
+    }).get();
+
+    if (dynamicClasses.length > 0) {
+        setTimeout(function () {
+            var svgObject = document.getElementById('svgObject');
+            var svgDoc = svgObject.contentDocument;
+
+            if (svgDoc) {
+                var allPaths = $(svgDoc).find('path');
+                allPaths.each(function () {
+                    var element = $(this);
+                    dynamicClasses.forEach(function (className) {
+                        if (element.hasClass(className)) {
+                            element.removeClass(className);
+                        }
+                    });
+                });
+            } else {
+                console.log('SVG content not yet loaded.');
+            }
+        }, 100);
+    }
+
+    bodyPart.remove();
+    console.log(dynamicClasses);
+});
+
+
+    // $("body").on("click", ".removeBodyPart", function () {
+    //     console.log('click');
+    //     var bodyPart = $(this).closest('.bodyPart');
+    //     var titleVal = bodyPart.find("input.bodyTitle").val();
+    //     var buttonRemove = bodyPart.find(".removeAccent");
+    //     console.log(bodyPart);
+    //     console.log(titleVal);
+    //     // bodyPart.remove();
+    // });
+
 
     $("body").on("click", ".addAccent", function () {
         var bodyPart = $(this).closest('.bodyPart');
         var accentArea = bodyPart.find('.accentArea');
-        var inputElement = bodyPart.find('input[type="text"]');
+        var inputElement = bodyPart.find('.bodyTitle');
         var title = inputElement.val();
      
         if (title == '') {
@@ -177,7 +397,7 @@
         if (title) {
     var countVal = 0;
 
-    $(".bodyPart input[type='text']").each(function () {
+    $(".bodyPart input[type='text'] .bodyTitle").each(function () {
         if ($(this).val().trim() === title.trim()) {
             countVal++;
         }
@@ -204,7 +424,7 @@
                                     <div class="form-group">
                                         <label class="form-label" for="accentTitle${accentCount}">Accent Title</label>
                                         <div class="form-control-wrap">
-                                            <input type="text" class="form-control" id="accentTitle${accentCount}" placeholder="Accent Title">
+                                            <input type="text" class="form-control ${title} accetntTitle" id="accentTitle${accentCount}" placeholder="Accent Title">
                                         </div>
                                     </div>
                                     <div class="form-group">
@@ -220,7 +440,7 @@
                                         @endforeach
                                     </div>
                                     <div class="form-group">
-                                        <button type="button" class="btn btn-outline-success">Select Image Area</button>
+                                        <button type="button" class="btn btn-outline-success selectImageArea" data-id="" data-class="${title}" data-title="accentTitle${accentCount}">Select Image Area</button>
                                         <button type="button" class="btn btn-outline-danger removeAccent">Remove Accent</button>
                                     </div>
                                 </div>
@@ -239,6 +459,26 @@
 
     $("body").on("click", ".removeAccent", function () {
         var accent = $(this).closest('.accordion');
+        var Dclass = $(this).attr('dinamicclass');
+
+        if (Dclass) {
+        setTimeout(function () {
+            var svgObject = document.getElementById('svgObject');
+            var svgDoc = svgObject.contentDocument;
+
+            if (svgDoc) {
+                var allPaths = $(svgDoc).find('path');
+                allPaths.each(function () {
+                    if ($(this).hasClass(Dclass)) {
+                        $(this).removeClass(Dclass);
+                    }
+                });
+            } else {
+                console.log('SVG content not yet loaded.');
+            }
+        }, 100);
+        }
+
         accent.remove();
     });
 
